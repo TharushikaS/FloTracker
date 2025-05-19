01cf942c91806f4ce2538b5d08f0690da5771348
Tharushika Surasinghe <surasinghe_rldth_e23@engug.ruh.ac.lk>
2025-05-19 23:46:04 +0530

    create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';      // Your global styles (including Tailwind)
+import App from './App';   // Your main App component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
